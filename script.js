7064413420cfa35e891827744f2dc0b91552cbcf
Sean Goudie <sgoudie77@hotmail.com>
2022-03-18 23:28:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Sticky Header 
+
+// Particle JS Hero Background
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles.js config loaded');
+// });
+
+// Responsive Menu Toggle
+
+// Text Typer
+var typed = new Typed(".typing", {
+    strings: ["changing.", "growing.", "evolving.", "right-sizing."],
+    typeSpeed: 300,
+    backSpeed: 80,
+    loop: true
+});
+
+// Appear on Scroll
+
+// Swiper Gallery - Portfolio
+
+// Owl Gallery - Testimonials
